880d767acc0cb9afc5b663e77f2a609a0dd58705
dkirby13 <dominic.kirby13@gmail.com>
2017-02-16 19:39:45 -0800

    updated post.java

--- Post.java
+++ Post.java
@@ -1,7 +1,21 @@
-package com.g13.shotgun;
+package edu.ucsb.cs.cs185.dkirby.awshelp3;
+
+/**
+ * Created by Dominic on 2/16/2017.
+ */
+
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBIndexRangeKey;
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBTable;
 
 import java.sql.Time;
 import java.util.Date;
+
+
+
+import java.sql.Time;
+import java.util.Date;
+import java.util.Random;
+
 import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.*;
 
 /**
@@ -9,28 +23,45 @@ import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.*;
  */
 
 
-@DynamoDBTable(tableName = "My_books")
+@DynamoDBTable(tableName = "Shotgun_Posts")
 public class Post {
     private String city;
-    private Date the_date;
-    private Time the_time;
+    //private Date the_date;
+    private int the_date;
+   // private Time the_time;
     private String user;
+    private String key;
 
-    @DynamoDBIndexRangeKey(attributeName = "Date")
-    public void set_date(Date d){ the_date = d;}
-    public Date get_date(){ return the_date;}
+    public Post()
+    {
+        Random r = new Random(10);
+        key = Integer.toString(r.nextInt());
+
+    }
+
+    @DynamoDBHashKey(attributeName = "Key")
+    public String get_key(){ return key;}
+    public void set_key(String d){ key = d;}
+
+
+    @DynamoDBIndexHashKey(attributeName = "Date")
+    public int get_date(){ return the_date;}
+    public void set_date(int d){ the_date = d;}
+
 
     @DynamoDBIndexRangeKey(attributeName = "City")
-    public void set_city(String c){ city = c;}
     public String get_city(){
         return city;
     }
+    public void set_city(String c){ city = c;}
 
-    @DynamoDBIndexRangeKey(attributeName = "Time")
+
+    /*@DynamoDBIndexRangeKey(attributeName = "Time")
     public void set_time(Time t){ the_time = t;}
-    public Time get_time(){ return the_time;}
+    public Time get_time(){ return the_time;}*/
 
-    @DynamoDBIndexRangeKey(attributeName = "User")
-    public void set_user(String u){ user = u;}
+    @DynamoDBAttribute(attributeName = "User")
     public String get_user(){ return user;}
+    public void set_user(String u){ user = u;}
+
 }
