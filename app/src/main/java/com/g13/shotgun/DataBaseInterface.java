de8fd0055ff5b44e1609b3248a4d6288de1291de
dkirby13 <dominic.kirby13@gmail.com>
2017-02-22 15:36:06 -0800

    made them threads

--- app/src/main/java/com/g13/shotgun/DataBaseInterface.java
+++ app/src/main/java/com/g13/shotgun/DataBaseInterface.java
@@ -8,12 +8,14 @@ import com.amazonaws.regions.Regions;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by Dominic on 2/16/2017.
  */
 
 public class DataBaseInterface {
+    List<Post> posts;
     /*CognitoCachingCredentialsProvider credentialsProvider = new CognitoCachingCredentialsProvider(
             getApplicationContext(),
             "us-west-2:a1e05d5b-80d8-4be4-afdd-8fe55238156d", // Identity Pool ID
@@ -36,16 +38,32 @@ public class DataBaseInterface {
     }
 
 
-    public List<Post> get_posts(int n)
+    public List<Post> get_posts()
     {
 
-        List<Post> posts = mapper.scan(Post.class, new DynamoDBScanExpression());
+        Runnable runnable = new Runnable() {
+            public void run() {
+                posts = mapper.scan(Post.class, new DynamoDBScanExpression());
+            }
+        };
+
+        Thread mythread = new Thread(runnable);
+        mythread.start();
+        //TimeUnit.SECONDS.sleep(2);
+        android.os.SystemClock.sleep(1000);
         return posts;
     }
 
-    public void push_post(Post p)
-    {
-        mapper.save(p);
+    public void push_post(final Post p) {
+        Runnable runnable = new Runnable() {
+            public void run() {
+                mapper.save(p);
+
+            }
+        };
+        Thread mythread = new Thread(runnable);
+        mythread.start();
+
     }
 
     public void push_posts(List<Post> posts)
