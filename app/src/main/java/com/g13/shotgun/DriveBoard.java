af1ff098bb4b6e27d634009ab9b57f6ce033bd46
koa-sato <koa.sato@gmail.com>
2017-02-22 18:16:09 -0800

    Added AndroidManifest.xml change

--- app/src/main/java/com/g13/shotgun/DriveBoard.java
+++ app/src/main/java/com/g13/shotgun/DriveBoard.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -17,20 +16,18 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import com.amazonaws.auth.CognitoCachingCredentialsProvider;
 import com.amazonaws.regions.Regions;
 
-import java.sql.Time;
 import java.util.ArrayList;
-import java.util.Date;
 
 public class DriveBoard extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
   ArrayList<Post> posts;
     ArrayList<Post> d_posts;
+
 
     public void updateList(ArrayList<Post> posts){
         ArrayAdapter<Post> postAdapter = new ArrayAdapter<Post>(this,
