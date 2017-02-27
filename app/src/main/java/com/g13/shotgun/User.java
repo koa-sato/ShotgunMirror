adb41a250a40da82995ac2975390232f3259b17d
koa-sato <koa.sato@gmail.com>
2017-02-26 17:50:48 -0800

    Added User.java

---
+++ app/src/main/java/com/g13/shotgun/User.java
@@ -0,0 +1,36 @@
+package com.g13.shotgun;
+
+public class User {
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String identityId;
+
+    public User() {
+        firstName = "First Name";
+        lastName = "Last Name";
+        email = "Email";
+        identityId = "";
+    }
+
+    public User(String _firstName, String _lastName, String _email, String _identityId) {
+        firstName = _firstName;
+        lastName = _lastName;
+        email = _email;
+        identityId = _identityId;
+    }
+
+    private String getFirstName() { return firstName; }
+    private void setFirstName(String newFirsName) { firstName = newFirsName; }
+
+    private String getLastName() { return lastName; }
+    private void setLastName(String newLastName) { lastName = newLastName; }
+
+    private String getEmail() { return email; }
+    private void setEmail(String newEmail) { email = newEmail; }
+
+    private String getIdentityID() { return identityId; }
+    private void setIdentityID(String newIdentityId) { identityId = newIdentityId; }
+
+}
