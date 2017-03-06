d741fc2c29d8e904d042fb2cfdf163cfc65c7345
Joseph <jkompella@umail.ucsb.edu>
2017-03-06 03:58:55 -0800

    Nicer UI for Post Forums

--- app/src/main/java/com/g13/shotgun/rideboard/CreateRideBoardPostActivity.java
+++ app/src/main/java/com/g13/shotgun/rideboard/CreateRideBoardPostActivity.java
@@ -7,8 +7,10 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.amazonaws.mobile.AWSConfiguration;
@@ -21,6 +23,7 @@ public class CreateRideBoardPostActivity extends AppCompatActivity {
     static final int TIME_DIALOG_ID = 666;
     DatePicker tp;
     DatePicker dp;
+    Spinner bags;
     int day;
     int month;
     int year;
@@ -43,6 +46,8 @@ public class CreateRideBoardPostActivity extends AppCompatActivity {
         final Button b = (Button) findViewById(R.id.add_button);
         final Button s = (Button) findViewById(R.id.sets);
         final Button se = (Button) findViewById(R.id.sete);
+        bags = (Spinner) findViewById(R.id.bags);
+
         //TimePicker tp = (TimePicker) findViewById(R.id.tptp);
         final TextView tx = (TextView) findViewById(R.id.city);
         day = dp.getDayOfMonth();
@@ -58,6 +63,14 @@ public class CreateRideBoardPostActivity extends AppCompatActivity {
         //calendar.set(year, month, day);
         //final Date the_date = calendar.getTime();
         //final Time the_time = new Time(tp.getCurrentHour(), tp.getCurrentMinute(),0);
+
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.bags_choices, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        bags.setAdapter(adapter);
 
         s.setOnClickListener(new View.OnClickListener() {
             @Override
