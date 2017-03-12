f9068304560a8fdbc0c8d7faee2e4b1ddd69effa
Dominic <dkirby@umail.ucsb.edu>
2017-03-12 11:31:49 -0700

    added user to user stuff

---
+++ app/src/main/java/com/g13/shotgun/ViewUserActivity.java
@@ -0,0 +1,46 @@
+package com.g13.shotgun;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class ViewUserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_user);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        TextView name = (TextView) findViewById(R.id.the_name);
+        Intent i = getIntent();
+        String the_user_name = i.getStringExtra("User");
+        name.setText(the_user_name);
+        Button b = (Button) findViewById(R.id.confirm);
+        b.setText("Confirm Match");
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "One day", Toast.LENGTH_SHORT);
+
+            }
+        });
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+}
