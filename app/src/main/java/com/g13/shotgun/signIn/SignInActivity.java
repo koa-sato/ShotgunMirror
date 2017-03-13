628040a9bcb27bf43d4a092e3ef25ade0aab92d0
nikhil-patil <p.nikhil97@gmail.com>
2017-03-12 20:32:10 -0700

    added homescreen

--- app/src/main/java/com/g13/shotgun/signIn/SignInActivity.java
+++ app/src/main/java/com/g13/shotgun/signIn/SignInActivity.java
@@ -16,9 +16,9 @@ import com.amazonaws.mobile.user.IdentityProvider;
 import com.amazonaws.mobile.user.signin.CognitoUserPoolsSignInProvider;
 import com.amazonaws.mobile.user.signin.SignInManager;
 import com.g13.shotgun.DatabaseUser;
+import com.g13.shotgun.Opening_Screen;
 import com.g13.shotgun.R;
 import com.g13.shotgun.UserDatabaseInterface;
-import com.g13.shotgun.driveboard.DriveBoard;
 import com.sendbird.android.SendBird;
 
 
@@ -70,7 +70,7 @@ public class SignInActivity extends Activity {
                 @Override
                 public void run() {
                     Log.d(LOG_TAG, "Launching Main Activity...");
-                    Intent i = new Intent(SignInActivity.this, DriveBoard.class);
+                    Intent i = new Intent(SignInActivity.this, Opening_Screen.class);
                     i.putExtra("parent class", SignInActivity.class.toString());
                     startActivity(i
                             .setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
